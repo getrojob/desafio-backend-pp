ce6f9c86a053e8db1dc2a98920982b80f8cfbde7
getrojob <getrojob@gmai.com>
2023-08-16 23:26:47 -0300

    feat: add Error Handling

---
+++ src/main/java/com/picpaysimplificado/infra/ControllerExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.picpaysimplificado.infra;
+
+import com.picpaysimplificado.dtos.ExceptionDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.apache.coyote.Response;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException ex) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Duplicate entry", "400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(EntityNotFoundException ex) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralException(Exception ex) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO(ex.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+
+}
