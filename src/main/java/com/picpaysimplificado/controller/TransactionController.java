dcb37788104b7419924924f2f9b1afdb5ebecebf
getrojob <getrojob@gmai.com>
2023-08-16 23:30:49 -0300

    feat: add ControllerTransaction, rote and DTO

---
+++ src/main/java/com/picpaysimplificado/controller/TransactionController.java
@@ -0,0 +1,29 @@
+package com.picpaysimplificado.controller;
+
+import com.picpaysimplificado.domain.transaction.Transaction;
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.dtos.TransactionDTO;
+import com.picpaysimplificado.dtos.userDTO;
+import com.picpaysimplificado.service.TransactionService;
+import com.picpaysimplificado.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transaction) throws Exception {
+        Transaction newTransaction = this.transactionService.createTransaction(transaction);
+        return new ResponseEntity<>(newTransaction, HttpStatus.OK);
+    }
+
+}
